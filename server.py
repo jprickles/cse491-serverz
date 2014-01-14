0f820515c6bb2d2d18b494175dfd93be43acc6e8
Ethan Ettema <ettemaet@arctic.cse.msu.edu>
2014-01-14 14:25:43 -0500

    Now performs HTTP request

--- server.py
+++ server.py
@@ -17,7 +17,13 @@ print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
+    print c.recv(1000)
     print 'Got connection from', client_host, client_port
+    c.send('HTTP/1.0 200 OK \r\n')
+    c.send('Content-Type:text/html \r\n')
+    c.send('Connection:close\r\n')
+    c.send('\r\n')
+    c.send("<h1>Hello, world!</h1> this is ettemaet's server\n")
     c.send('Thank you for connecting')
     c.send("good bye.")
     c.close()
